ed708aaad4e81e90420ef89fd1fe2d5b7fcfb2ea
chenhongjun <1769003060@qq.com>
2016-11-23 22:38:38 +0800

    test

--- push.sh
+++ push.sh
@@ -5,6 +5,7 @@
 # Created Time: 2016年11月23日 星期三 21时49分36秒
 #########################################################################
 #!/bin/bash
+echo $1
 git add .
-git commit -m "$1"
+git commit -m $1
 git push -u origin master
